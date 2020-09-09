ed7e8d3067508f639d16d4deedf25a1598ec5e0e
dimitri <dimitri.torfs@gmail.com>
2020-09-09 22:08:15 +0200

    add example code

---
+++ codecov-sample/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn covered(i: u32) -> u32  {
+    i * 2
+}
+
+pub fn uncovered(i: u32) -> u32  {
+    i * 3 
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_covered() {
+        assert_eq!(covered(1), 2);
+    }
+
+}
